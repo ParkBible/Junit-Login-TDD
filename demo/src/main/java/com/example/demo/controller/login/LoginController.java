ca234a72d08ab47ce69b446ec2d6c760b543fa97
parkbible <p8yft8903@gmail.com>
2023-07-01 18:15:56 +0900

    d

--- demo/src/main/java/com/example/demo/controller/login/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/login/LoginController.java
@@ -16,7 +16,6 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<Integer> login(LoginRequest loginRequest) {
-        System.out.println("로그인을 합니다.");
         return loginService.login(loginRequest);
     }
 }
