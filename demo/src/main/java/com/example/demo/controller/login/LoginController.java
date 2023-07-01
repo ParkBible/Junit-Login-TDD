83d74933546d73ce0582057670ea1fdd9f386df4
parkbible <p8yft8903@gmail.com>
2023-07-01 18:13:08 +0900

    시작하겠습니다

--- demo/src/main/java/com/example/demo/controller/login/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/login/LoginController.java
@@ -16,6 +16,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<Integer> login(LoginRequest loginRequest) {
+        System.out.println("로그인을 시작하겠습니다.");
         return loginService.login(loginRequest);
     }
 }
